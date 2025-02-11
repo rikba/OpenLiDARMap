709ff06061a4a1fedd2a241014c0953b5041827e
Dominik Kulmer <dominik.kulmer@tum.de>
2025-02-11 14:59:04 +0100

    Docker tags for release

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-tag=latest
+tag=0.1.0
 
 docker build -f $SCRIPT_DIR/../Dockerfile -t ga58lar/openlidarmap:$tag $SCRIPT_DIR/..
